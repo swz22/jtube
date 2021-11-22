18d772f4bae99e1981891e187ce7b1280c8b2661
John Kim <jkim005@yahoo.com>
2021-11-22 14:06:07 -0800

    removed unnecessary component and updated styling

--- src/client/src/app/app-routing.module.ts
+++ src/client/src/app/app-routing.module.ts
@@ -3,15 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { CollectionComponent } from './components/collection/collectioncomponent';
 import { UploadPageComponent } from './components/upload-page/upload-page.component';
-import { SignupComponent } from './components/signup/signup.component';
 import { VideoplayerComponent } from './components/videoplayer/videoplayer.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full'},
+  // { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
   { path: 'collection', component: CollectionComponent },
   { path: 'upload', component: UploadPageComponent },
-  { path: 'signup', component: SignupComponent },
   { path: 'stream/:filename', component: VideoplayerComponent},
 ];
 
--- src/client/src/app/app.module.ts
+++ src/client/src/app/app.module.ts
@@ -8,7 +8,6 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { CollectionComponent } from './components/collection/collectioncomponent';
-import { SignupComponent } from './components/signup/signup.component';
 import { UploadPageComponent } from './components/upload-page/upload-page.component';
 import { VideoplayerComponent } from './components/videoplayer/videoplayer.component';
 
@@ -27,7 +26,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     NavBarComponent,
     UploadPageComponent,
     CollectionComponent,
-    SignupComponent,
     VideoplayerComponent,
   ],
   imports: [
